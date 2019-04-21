539d125514468743bddc2a2767b1aad91c8abebb
Austin Bundy <aTmb405@gmail.com>
2019-04-20 20:50:30 -0500

    tab icon and navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar";
+import Quotes from "./Quotes";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHashtag, faRedoAlt } from '@fortawesome/free-solid-svg-icons';
+import './QuotesApp.css';
+
+library.add(faHashtag);
+library.add(faRedoAlt);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" id="quote-box">
+        <Navbar />
+        <Quotes />
       </div>
     );
   }
---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Navbar.css';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <header>
+        <h2><a href="#">Travel Quotes</a></h2>
+        <nav>
+          <li><a href="#">aTmb405</a></li>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default Navbar;
